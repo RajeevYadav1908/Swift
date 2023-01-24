b6c8052217bee855349225931f1be2e959c4c5b4
Rajeev Yadav <rajeevyadav0123@Rajeevs-MacBook-Pro.local>
2023-01-24 12:19:09 +0530

    Initial Setup for Swift Airline App.

---
+++ AirlineApplication/AirlineApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirlineApplication
+//
+//  Created by Rajeev Yadav on 24/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

738d8c7493836882b424e43dde363884acff4489
Rajeev Yadav <rajeevyadav0123@Rajeevs-MacBook-Pro.local>
2023-06-05 10:22:37 +0530

    Banner View

---
+++ BannerToastView/BannerToastView/BannerToastViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BannerToastViewApp.swift
+//  BannerToastView
+//
+//  Created by Rajeev Yadav on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BannerToastViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BannerToastView/BannerToastView/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  BannerToastView
+//
+//  Created by Rajeev Yadav on 03/06/23.
+//
+
+import SwiftUI
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+
+struct CustomScreen: View {
+    
+    @State private var showView: Bool = false
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            
+            VStack {
+                Spacer()
+                Button("Show Alert") {
+                    withAnimation {
+                        showView.toggle()
+                    }
+                }
+                .modifier(Title())
+                Spacer()
+            }
+            
+            if showView {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.gray)
+                        .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.1)
+                        .transition(.asymmetric(insertion: .move(edge: .top), removal: .move(edge: .top)))
+                        // you can add this effect also removal: AnyTransition.identity.animation(.easeInOut)
+                }
+                
+                VStack{
+                    Text("Alert Banner!!!")
+                        .padding(10)
+                    Text("Content to display!!")
+                        .onReceive(timer) { input in
+                            showView = false
+                        }
+
+
+                }
+                
+            }
+            
+            
+        }
+    }
+}
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            CustomScreen()
+        }
+//        .animation(.easeInOut, value: 20)
+        .transition(.move(edge: .top))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
